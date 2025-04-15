14a5876de9501ba0c559f1f27312de5030372492
Albin Baby <albinbabayme05@gmail.com>
2025-04-15 21:59:30 +0200

    MOvie class defined

---
+++ MovieBookingSystem/Movie.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieBookingSystem
+{
+    public class Movie
+    {
+        private string MovieId;
+        private string MovieName { get; set; }
+        private string Duration;
+        private string Genre;
+
+        public List<string> ShowTime;
+        public Movie(string name, string movieId, string duration, string genre)
+        {
+            MovieName = name;
+            MovieId = movieId;
+            Duration = duration;
+            Genre = genre;
+            ShowTime = new List<string>();
+        }
+
+
+        public String GetMovieName() => MovieName;
+        public string GetMovieId() => MovieId;
+
+        public void DisplayMovieInfo()
+        {
+            Console.WriteLine($"MovieName: {MovieName} - Duration: {Duration} - Genre: {Genre} ");
+        }
+        public void GetAvailbleShowtime()
+        {
+            foreach (var show in ShowTime)
+            {
+                Console.WriteLine($"shows: {show}");
+            }
+        }
+    }
+
+}
+
+
---
+++ MovieBookingSystemTester/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using MovieBookingSystem;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Movie movie = new Movie("Sevens", "s7", "2h30m", "thriller");
+
+        movie.DisplayMovieInfo();
+        movie.ShowTime.Add("12pm, 6pm, 9pm");
+        movie.GetAvailbleShowtime();
+    }
+}
+
