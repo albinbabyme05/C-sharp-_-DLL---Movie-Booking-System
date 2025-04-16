cd0475c95256ee3fb7e8beb11c1ce71eb969180d
Albin Baby <albinbabayme05@gmail.com>
2025-04-16 14:29:08 +0200

    called all methods and ensured working properly

--- MovieBookingSystemTester/Program.cs
+++ MovieBookingSystemTester/Program.cs
@@ -7,8 +7,8 @@ class Program
     static void Main(string[] args)
     {
         Movie movie1 = new Movie("Sevens", "s7", "2h30m", "thriller");
-        Movie movie2 = new Movie("Sevens", "s7", "2h30m", "thriller");
-        Movie movie3 = new Movie("Sevens", "s7", "2h30m", "thriller");
+        Movie movie2 = new Movie("inception", "I3", "1h15m", "KRIMI");
+        Movie movie3 = new Movie("kali", "KM7", "3H", "Drama");
         Theatre t1 = new Theatre("PVR", "Kochi");
 
         t1.AddMovie(movie1);
@@ -27,14 +27,23 @@ class Program
         t1.SetSeatAvailbilty(movie1, "3pm", 50);
         t1.SetSeatAvailbilty(movie1, "2pm", 50);
         t1.SetSeatAvailbilty(movie1, "9pm", 70);
+        Console.WriteLine("Available Movies:");
+
+        foreach (var movie in t1.AvailableMovies)
+        {
+            movie.DisplayMovieInfo();
+            var times = t1.GetShowTime(movie);
+            Console.WriteLine("Showtimes: " + string.Join(", ", times));
+        }
 
         t1.BookSeat(movie1, "3pm", 15);
-        t1.BookSeat(movie1, "2pm", 3);
+        t1.BookSeat(movie1, "1pm", 3);
         t1.BookSeat(movie1, "9pm", 20);
 
-        t1.CheckSeatAvailbilty(movie1, "9pm");
- 
-        
+        Console.WriteLine($"Remaining Seats for {movie1.GetMovieName()}   {t1.CheckSeatAvailbilty(movie1, "3pm")}");
+        Console.WriteLine($"Remaining Seats for {movie1.GetMovieName()}   {t1.CheckSeatAvailbilty(movie1, "9pm")}");
+
+
     }
 }
 
