a99aaf029c7da7b5946fb3545866c9805b9c54f2
Albin Baby <albinbabayme05@gmail.com>
2025-04-17 09:47:45 +0200

    defined a abstract class implimented for users

--- MovieBookingSystem/User.cs
+++ MovieBookingSystem/User.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace MovieBookingSystem
 {
-    internal class User
+    public abstract class User
     {
+        private string UserName { get; set; }
+        private Guid UserId { get; set; }
+        private string Email { get; set; }
+
+        protected User(string userName, string email)
+        {
+            UserName = userName;
+            UserId = Guid.NewGuid();
+            Email = email;
+        }
+
+        public abstract List<Movie> SearchMovie(string movieName, List<Movie> movielist);
+
+        public abstract List<Theatre> ViewTheatre(Movie movie, List<Theatre> theatresList);
+
+        public abstract string BookTicket(Theatre theatre, Movie movie, string timing, int seats);
     }
 }
